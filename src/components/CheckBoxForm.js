48d2997d240f95c091e9544cf17c5b5745ec1d5a
장정안 <wjddks96@naver.com>
2023-07-26 14:30:31 +0900

    refactor: 체크박스 폼 체크여부 수정

--- src/components/CheckBoxForm.js
+++ src/components/CheckBoxForm.js
@@ -4,8 +4,12 @@ export default function CheckBoxForm() {
   const [isCheck, setIsCheck] = useState(false);
 
   const isCheckConfirm = (e) => {
-    console.log(e.target.checked);
-    setIsCheck(true);
+    console.log(e.target.checked); //TODO: 삭제
+    if(e.target.checked) {
+        setIsCheck(true);
+    } else {
+        setIsCheck(false);
+    }
   };
 
   return (
