b3470676ed241f8681b62a57c79ee47fc9c07a61
Seongsuk Hong <ssukh77@naver.com>
2023-07-26 14:54:13 +0900

    design: 회원가입 UI 추가

---
+++ src/components/Join.js
@@ -0,0 +1,122 @@
+import React from "react";
+
+const Join = () => {
+
+  return (
+    <div className="join_container">
+      <h2>회원가입</h2>
+      <form>
+        <div className="input_item">
+          <label htmlFor="userID" className="input_title">
+            아이디
+          </label>
+          <input
+            type="text"
+            id="userID"
+            className="joinInput"
+            minLength={6}
+            maxLength={20}
+            placeholder="소문자, 숫자 조합 6-20자"
+          />
+          {}
+          <button type="button" className="">
+            중복검사
+          </button>
+        </div>
+        <div className="input_item">
+          <label htmlFor="userPW" className="input_title">
+            비밀번호
+          </label>
+          <input
+            type="text"
+            id="userPW"
+            minLength={8}
+            maxLength={64}
+            className="joinInput"
+            placeholder="영문, 숫자, 특수문자(@, #, $, !, %, *, ?, & 하나 이상) 조합 8-16자"
+          />
+        </div>
+        <div className="input_item">
+          <label htmlFor="pwCheck" className="input_title">
+            비밀번호 확인
+          </label>
+          <input
+            type="text"
+            id="pwCheck"
+            minLength={8}
+            maxLength={64}
+            className="joinInput"
+            placeholder="영문, 숫자, 특수문자(@, #, $, !, %, *, ?, &) 조합 8자 이상"
+          />
+        </div>
+        <div className="input_item">
+          <label htmlFor="phone" className="input_title">
+            휴대폰 번호
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            maxLength={15}
+            className="joinInput"
+            placeholder="010-1234-1234"
+          />
+        </div>
+        <div className="input_item">
+          <label htmlFor="address" className="input_title">
+            주소
+          </label>
+          <input
+            type="text"
+            id="address"
+            className="joinInput"
+          />
+          <button type="button">주소검색</button>
+
+          <label htmlFor="detailAddress">상세주소</label>
+          <input
+            type="text"
+            id="detailAddress"
+            placeholder="상세주소"
+          />
+        </div>
+        <div className="input_item">
+          <label htmlFor="address" className="input_title">
+            이메일
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="joinInput"
+            placeholder=""
+          />
+          @
+          <input
+            type="text"
+            className="joinInput"
+            placeholder="직접입력"
+            list="domainList"
+          />
+          <datalist id="domainList">
+          </datalist>
+        </div>
+        <div className="">
+          <label htmlFor="agree" className="">
+            <input
+              type="checkbox"
+              id="agree"
+            />
+            &#91;필수&#93; 만 14세 이상이며 모두 동의합니다.
+          </label>
+        </div>
+        <button
+          type="submit"
+          className="joinBtn"
+        >
+          가입하기
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Join;
